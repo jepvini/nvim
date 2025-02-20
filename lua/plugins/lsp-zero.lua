f8797ca63d79c58d1e661bfa3cb05856b3b7e364
leo <leos1359531@gmail.com>
2025-02-20 08:41:23 +0100

    added zig lsp

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -184,6 +184,10 @@ return {
         capabilities = capabilities,
         on_attach = on_attach,
       })
+      lspconfig.zls.setup({
+        capabilities = capabilities,
+        on_attach = on_attach,
+      })
     end,
   },
 }
